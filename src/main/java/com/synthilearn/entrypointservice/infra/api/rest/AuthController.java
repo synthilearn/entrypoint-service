39b7696430d72edbbb7ed66846839ea400b3564b
Nikita <nik.porshennikov@mail.ru>
2024-02-11 16:30:28 +0700

    Added cors config 2

--- src/main/java/com/synthilearn/entrypointservice/infra/api/rest/AuthController.java
+++ src/main/java/com/synthilearn/entrypointservice/infra/api/rest/AuthController.java
@@ -14,6 +14,7 @@ import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/entrypoint-service/v1/auth")
+@CrossOrigin(origins = "*")
 @RequiredArgsConstructor
 public class AuthController {
 
--- src/main/java/com/synthilearn/entrypointservice/infra/api/rest/Oauth2Controller.java
+++ src/main/java/com/synthilearn/entrypointservice/infra/api/rest/Oauth2Controller.java
@@ -8,10 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpResponse;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
@@ -22,6 +19,7 @@ import java.util.Objects;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 @RequestMapping("/entrypoint-service/v1/oauth2")
 public class Oauth2Controller {
 
